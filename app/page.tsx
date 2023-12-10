750b8fbf453af4dd9651f7647e7d449228d7e50f
Mario Gómez <ma.gomez@duocuc.cl>
2023-12-09 22:07:55 -0300

    Design improvements

--- app/page.tsx
+++ app/page.tsx
@@ -38,7 +38,7 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center p-5 md:p-24">
-      <h1 className="text-4xl font-bold text-center mb-20">
+      <h1 className="text-xl md:text-4xl font-bold text-center mb-20">
         localStorage testing
       </h1>
         <form className="flex gap-3" onSubmit={handleSubmit}>
@@ -53,7 +53,7 @@ export default function Home() {
           <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' type='submit'>Submit</button> 
         </form>
         <h2 className="text-xl text-bold text-center mt-20">
-          Your localStorage saved items
+          Your localStorage items
         </h2>
 
         {items.length > 0 ? (
@@ -77,7 +77,7 @@ export default function Home() {
           <p className="mt-5">You dont have anything saved.</p>
         )}
 
-        <button className='bg-slate-500 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded mt-5' onClick={handleClear}>Clear {items.length} localStorage items</button>
+        <button className='bg-slate-500 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded mt-5' onClick={handleClear}>Clear {items.length} items</button>
     </main>
   )
 }
